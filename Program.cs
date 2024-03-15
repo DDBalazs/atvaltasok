96163060951345e97c004a0db39bf9c88c5b6bcb
Balázs <Balzs@DESKTOP-ID885J2>
2024-03-15 12:09:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Átváltások
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérlek adj meg egy hőmérséklet értéket: ");
+            double homerseklet = int.Parse(Console.ReadLine());
+            Console.Write("Kérlek mond meg hogy Celsiusból Farenheitbe vagy Farenheitből Celsiusba szeretnéd váltani (CF-FC): ");
+            string valasz = Console.ReadLine();
+            if (valasz == "CF")
+            {
+                Console.WriteLine($"Farenheit: {homerseklet*1.8000+32}°F");
+            }
+            else if (valasz == "FC")
+            {
+                Console.WriteLine($"Celsius: {(homerseklet-32)/1.8000:00.000}°C");
+            }
+            else
+            {
+                Console.WriteLine("nem jó választ adtál");
+            }
+        }
+    }
+}
